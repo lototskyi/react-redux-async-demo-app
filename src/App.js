6f75a4e83fe6ceec29ab635476c4680785fb0b9f
alex <o.lototskyi@gmail.com>
2023-05-25 09:42:27 +0300

    using useEffect with redux for http requests

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -7,6 +8,11 @@ import Products from "./components/Shop/Products";
 function App() {
 
     const showCart = useSelector(state => state.ui.cartIsVisible);
+    const  cart = useSelector(state => state.cart);
+
+    useEffect(() => {
+        fetch('https://redux-cart-65759-default-rtdb.europe-west1.firebasedatabase.app/cart.json', { method: 'PUT', body: JSON.stringify(cart) });
+    }, [cart]);
 
     return (
         <Layout>
